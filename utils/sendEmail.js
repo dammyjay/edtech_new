c95fab49ab2405bec163e43dfb4ef93a70158158
dammyjay <dammykirchhoff@gmail.com>
2026-02-01 08:13:05 +0100

    Send mail fixes

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -146,30 +146,55 @@
 // ---------------------------------------------------------------------------------------------------
 
 
-const Brevo = require("@getbrevo/brevo");
+// const Brevo = require("@getbrevo/brevo");
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(
-  Brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
-);
+// const apiInstance = new Brevo.TransactionalEmailsApi();
+// apiInstance.setApiKey(
+//   Brevo.TransactionalEmailsApiApiKeys.apiKey,
+//   process.env.BREVO_API_KEY
+// );
 
+// async function sendEmail(to, subject, htmlContent) {
+//   try {
+//     const sendSmtpEmail = {
+//       to: [{ email: to }],
+//       sender: { email: process.env.BREVO_FROM, name: "JKT Hub" },
+//       subject,
+//       htmlContent,
+//     };
+
+//     const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+//     console.log("✅ Email sent:", data.messageId || data);
+//     return data;
+//   } catch (error) {
+//     console.error("❌ Email sending failed:", error.message);
+//     throw error;
+//   }
+// }
+
+// module.exports = sendEmail;
+
+
+// ---------------------------------------------------------------------------------------------------
+// utils/sendEmail.js
 async function sendEmail(to, subject, htmlContent) {
   try {
-    const sendSmtpEmail = {
+    const sendSmtpEmail = new Brevo.SendSmtpEmail({
       to: [{ email: to }],
-      sender: { email: process.env.BREVO_FROM, name: "JKT Hub" },
+      sender: {
+        email: process.env.BREVO_FROM,
+        name: "JKT Hub",
+      },
       subject,
       htmlContent,
-    };
+    });
 
     const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    console.log("✅ Email sent:", data.messageId || data);
+    console.log("✅ Email sent:", data.messageId);
     return data;
   } catch (error) {
-    console.error("❌ Email sending failed:", error.message);
+    console.error("❌ Email sending failed:", error);
     throw error;
   }
 }
-
 module.exports = sendEmail;
